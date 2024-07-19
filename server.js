f0a1e484c1eaf403071bbb5ef59915cfdec7412c
Fahrur Rifai <mfahrurrifai@gmail.com>
2024-07-19 16:52:44 +0700

    feat: Add CORS headers to server.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('tiny'));
 
+// add cors api
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(routes);
 
 mongoose
