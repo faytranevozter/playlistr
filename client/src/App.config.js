feb9fa4926cd17709a79e6d68347b7a24f1e3547
Fahrur Rifai <mfahrurrifai@gmail.com>
2024-07-19 16:01:02 +0700

    chore: Remove unused code and dependencies

--- client/src/App.config.js
+++
@@ -1,8 +0,0 @@
-const apiUrlOptions = {
-  prod: 'https://playlistr-io.herokuapp.com',
-  dev: 'http://localhost:8888',
-};
-
-const apiURL = window.location.hostname === 'localhost' ? apiUrlOptions.dev : apiUrlOptions.prod;
-
-export { apiURL };
